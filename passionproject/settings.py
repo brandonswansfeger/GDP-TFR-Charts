fa6db9dd768f7f7d02218f6cf65fe43887ea3957
brandonswansfeger <88030396+brandonswansfeger@users.noreply.github.com>
2022-01-06 03:55:09 -0500

    update

--- passionproject/settings.py
+++ passionproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ilm=%ns@g9434_2+%hx$t*ewguo^o3bd38&=8gix4eae5d=ud6'
+SECRET_KEY = config('SECRET_KEY')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,12 +82,24 @@ WSGI_APPLICATION = 'passionproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+  
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'passionproject',
+
+        'USER': 'bswansfeger',
+
+        'PASSWORD': 'Kitten2222!',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
