aaa1bcc7d3d88102300cbaf1f9d2147536fa1ebf
brandonswansfeger <88030396+brandonswansfeger@users.noreply.github.com>
2022-01-08 18:25:18 -0500

    update

--- gnibarchart/utils.py
+++ gnibarchart/utils.py
@@ -55,19 +55,18 @@ def get_plot(yearselected, indicatorselected):
     df4 = pd.DataFrame(TFRsorted)
     yearonly = yearselected[2:]
 
-# render bar chart and specefy styling
-    # df5 = df4.head(223)
+# render bar chart and specify styling
+  
     df6 = df4[~df4['Country'].isin(['Sub-Saharan Africa (excluding high income)', 'Post-demographic dividend', 'Pre-demographic dividend', 'Late-demographic dividend', 'Early-demographic dividend', 'Sub-Saharan Africa (IDA & IBRD countries)', 'South Asia (IDA & IBRD)', 'Middle East & North Africa (IDA & IBRD countries)', 'Latin America & the Caribbean (IDA & IBRD countries)', 'Europe & Central Asia (IDA & IBRD countries)', 'East Asia & Pacific (IDA & IBRD countries)', 'Africa Eastern and Southern', 'Africa Western and Central', 'Arab World', 'Central Europe and the Baltics', 'Caribbean small states', 'East Asia & Pacific (excluding high income)', 'Earlydemographic dividend', 'East Asia & Pacific', 'Europe & Central Asia (excluding high income)', 'Europe & Central Asia', 'Euro area', 'European Union', 'Fragile and conflict affected situations', 'High income', 'Heavily indebted poor countries (HIPC)', 'IBRD only', 'IDA & IBRD total', 'IDA total', 'IDA blend', 'IDA only', 'Latin America & Caribbean (excluding high income)', 'Latin America & Caribbean', 'Least developed countries: UN classification', 'Low income', 'Lower middle income', 'Low & middle income', 'Latedemographic dividend', 'Middle East & North Africa', 'Middle income', 'Middle East & North Africa (excluding high income)', 'North America', 'OECD members', 'Other small states', 'Predemographic dividend', 'Pacific island small states', 'Postdemographic dividend', 'South Asia', 'SubSaharan Africa (excluding high income)', 'SubSaharan Africa', 'Small states', 'East Asia & Pacific (IDA & IBRD)', 'Europe & Central Asia (IDA & IBRD)', 'Latin America & Caribbean (IDA & IBRD)', 'Middle East & North Africa (IDA & IBRD)', 'South Asia (IDA & IBRD)', 'SubSaharan Africa (IDA & IBRD)', 'Upper middle income', 'World'])]
     x = df6['Country'].to_numpy()
     y = df6[yearselected].to_numpy(np.float64)
-    # print(y)
+   
     arrvalues = np.array(y)
     values = arrvalues.tolist()
     idx = np.array(x)
-    print(values)
-    # bluecolors = ['#7FFFD4', '#0000FF', '#5F9EA0', '#6495ED', '#00FFFF', '#00008B', '#00CED1', '#00BFFF', '#1E90FF', '#ADD8E6', '#87CEFA', '#B0C4DE', '#0000CD', '#48D1CC', '#191970', '#000080', '#AFEEEE', '#4169E1', '#87CEEB', '#4682B4', '#40E0D0', ]
+ 
     fig, ax = plt.subplots()
-# ...0
+
     fig.tight_layout()
     colorschart = []
     for val in values:
@@ -78,7 +77,6 @@ def get_plot(yearselected, indicatorselected):
     plt.figure(figsize=(23,11))
     plt.bar(idx, values, color=colorschart)
            
-    # df6.plot.bar(x=x, y=y, figsize=(22,14), fontsize=26, color=my_colors, rot=90).grid(axis='y')
    
     plt.xticks(fontsize=9, rotation=90)
     plt.axhline(y=2.1, color='blue', linestyle='--')
